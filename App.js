c2e19d003a062ffa4987b2f7344b1a4c7d11081d
Filipedel <filipedelaunay@gmail.com>
2022-09-23 21:47:55 +0200

    .

--- App.js
+++ App.js
@@ -6,10 +6,35 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static('Front/build'))
 
+function generateRandomString(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const Client_ID = '9f0488cb2b5948e09954ff6bbfaa3c54';
+const redirect = "http://localhost.8888/first";
+const state = generateRandomString(16)
+const stateKey = 'spotify_auth_state';
+localStorage.setItem(stateKey, state);
+var scope = ['user-read-private user-read-email'];
+
+var url = 'https://accounts.spotify.com/authorize';
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(Client_ID);
+url += '&scope=' + encodeURIComponent(scope);
+url += '&redirect_uri=' + encodeURIComponent(redirect);
+url += '&state=' + encodeURIComponent(state);
+
+
 
 app.get("/first", (req,res)=>{
   res.send({
-    msg: "Start Front !"
+    msg: url
   })
 })
 
--- Front/src/App.js
+++ Front/src/App.js
@@ -2,41 +2,24 @@ import './App.css';
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
 
-function generateRandomString(length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-};
 
-const Client_Id = '9f0488cb2b5948e09954ff6bbfaa3c54';
-const secret = "381942fa91ea4627b34baa34c486800c";
-const redirect = "http://localhost.8888/first";
-const state = generateRandomString(16)
-const stateKey = 'spotify_auth_state';
-localStorage.setItem(stateKey, state);
-var scope = ['user-read-private user-read-email'];
+const  App = () => {
 
-function  App(){
+    const  [msg, setmsg] = useState("");
     const handleClick = async() => {
 
-        const data = await fetch('first');
-        var url = 'https://accounts.spotify.com/authorize';
-        url += '?response_type=token';
-        url += '&client_id=' + encodeURIComponent(Client_Id);
-        url += '&scope=' + encodeURIComponent(scope);
-        url += '&redirect_uri=' + encodeURIComponent(redirect);
-        url += '&state=' + encodeURIComponent(state);
+        const data = await fetch('/first');
+        const json = data.json();
+        const msg = json.msg
+        setmsg(msg)
 
     }
 
     return (
         <div className="container">
             <button onClick={handleClick}>login to spotify</button>
-            <p>{url}</p>
+            <p>{msg}</p>
             <button >Get play</button>
         </div>
     );
