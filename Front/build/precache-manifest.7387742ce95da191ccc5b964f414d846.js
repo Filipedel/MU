671916ec51bd13ca7f35c05c9c48b750a8ed3c5a
Filipedel <filipedelaunay@gmail.com>
2022-09-26 15:42:18 +0200

    .

---
+++ Front/build/precache-manifest.7387742ce95da191ccc5b964f414d846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96629b97e33b43a05d1",
+    "url": "/static/css/main.8bbe64d4.chunk.css"
+  },
+  {
+    "revision": "f96629b97e33b43a05d1",
+    "url": "/static/js/main.7fcc83d9.chunk.js"
+  },
+  {
+    "revision": "85dbeb6abf83115a29b6",
+    "url": "/static/js/runtime~main.70091e6f.js"
+  },
+  {
+    "revision": "d276a230ee3c75cb4e1b",
+    "url": "/static/js/2.36877055.chunk.js"
+  },
+  {
+    "revision": "d03d05430b9a42cc73e3",
+    "url": "/static/js/3.06a4cbf0.chunk.js"
+  },
+  {
+    "revision": "4e8684c3aa167b8dce0f33e6822d6cea",
+    "url": "/index.html"
+  }
+];
