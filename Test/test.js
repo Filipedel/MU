cb5dcbd95def1b6174471c9be3377d8915b156c7
Filipedel <filipedelaunay@gmail.com>
2022-10-28 21:06:52 +0200

    test fixed

--- Test/test.js
+++ Test/test.js
@@ -6,7 +6,7 @@ const request = require('request');
 
 describe("Get '/*'", function (){
   it("Main page should return 200",function (done){
-      request("/*",function (error,response,body){
+      request("/",function (error,response,body){
           expect(body).to.equal(undefined);
           done();
       })
